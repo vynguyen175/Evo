4637f645d141839611e73db4d84e78cd973c05fc
Vy Nguyen <vyn13217@gmail.com>
2025-12-19 23:59:25 -0500

    Add padding to Container component to fix white space on all pages

--- evo/src/components/ui/Container.tsx
+++ evo/src/components/ui/Container.tsx
@@ -7,7 +7,7 @@ interface ContainerProps {
 
 export default function Container({ children, className = '' }: ContainerProps) {
   return (
-    <div className={`max-w-7xl mx-auto ${className}`}>
+    <div className={`max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ${className}`}>
       {children}
     </div>
   );
